27a46c9ac7a6c7718dbab567a01a0edf450018f6
sunopar <xygjsj@139.com>
2016-10-28 16:13:05 +0800

    init test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-var js = require('./example.react')
-// console.log(js+'\n'+'hello world')
-console.log(js)
-=======
 require('./test.react')
->>>>>>> dev
--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-var webpack = require('webpack');
-var path = require('path');
-
-var clientConfig = {
-    entry:{
-        module:'./test',
-    },
-    output:{
-        path: path.resolve(__dirname, 'dist'),
-        filename:'[name].bundle.js'
-    },
-    devtool:'#source-map',
-    debug:true,
-    module:{
-        loaders:[
-            
-            {
-                test:/\.react$/,
-                loader:'react'
-            },
-            
-            {
-                test:/\.html$/,
-                loader:'raw'
-            },
-            
-            
-        ]
-    },
-    babel:{
-        presets:['es2015','react','stage-0'],
-        plugins:['transform-runtime']
-    }
-    
-}
-
-module.exports = clientConfig;
-=======
 var webpack = require('webpack')
 var path = require('path')
 module.exports={
@@ -57,4 +18,3 @@ module.exports={
         ]
     }
 }
->>>>>>> dev
